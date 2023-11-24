8f7ca5a9a107881d5e56dbb2e17a26a244c4c057
Promie Yutasane <promie.yutasane@gmail.com>
2023-11-25 06:53:41 +1100

    ND-07 constructed the first method to get all notes

--- src/notes/entities/note.entity.ts
+++ src/notes/entities/note.entity.ts
@@ -1,1 +1,19 @@
-export class Note {}
+export class Note {
+  noteId: string;
+  title?: string;
+  content: string;
+  createdAt: Date;
+  updatedAt?: Date;
+
+  static newInstanceFromDynamoDBObject(data: any): Note {
+    const result = new Note();
+    result.noteId = data.noteId.S;
+    result.title = data?.title?.S;
+    result.content = data.content.S;
+    result.createdAt = new Date(Number(data.createdAt.N));
+    if (data.updatedAt) {
+      result.updatedAt = new Date(Number(data.updatedAt.N));
+    }
+    return result;
+  }
+}
