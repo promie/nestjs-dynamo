84575f1f9e9da0258249dc2e31b92cdefe90b1ed
Promie Yutasane <promie.yutasane@gmail.com>
2023-11-25 13:41:03 +1100

    ND-13 defined the new DTO and the entity to POST

--- src/notes/dto/create-note.dto.ts
+++ src/notes/dto/create-note.dto.ts
@@ -1,1 +1,10 @@
-export class CreateNoteDto {}
+import { IsOptional, IsString } from 'class-validator';
+
+export class CreateNoteDto {
+  @IsString()
+  @IsOptional()
+  title?: string;
+
+  @IsString()
+  content: string;
+}
